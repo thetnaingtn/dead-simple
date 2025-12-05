30fb909c50911f250b25ca7f8062646255e87e68
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2025-12-05 14:17:59 +0700

    feat: add hello handler

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thetnaingtn/dead-simple
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	resp := struct {
+		Message string `json:"message"`
+	}{
+		Message: "Hello World!!",
+	}
+
+	bytes, err := json.Marshal(resp)
+	if err != nil {
+		panic(err)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(bytes)
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hello", helloHandler)
+
+	log.Printf("The server now start listening")
+	log.Fatal(http.ListenAndServe(":", mux))
+}
