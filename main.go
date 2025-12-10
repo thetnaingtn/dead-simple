baf66cf1749cc6dbf6503934e19e485d47269950
Thet Naing Tun <thetnaingtun@bindez.com>
2025-12-10 15:12:34 +0700

    feat: some tweak

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	resp := struct {
 		Message string `json:"message"`
 	}{
-		Message: "Hello World!!",
+		Message: "You see this changes because ArgoCD update me",
 	}
 
 	bytes, err := json.Marshal(resp)
